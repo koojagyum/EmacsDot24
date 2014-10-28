09469062173b4df9f4974c14fc9762d481d6610d
Jagyum Koo <koojagyum@gmail.com>
2014-10-28 11:17:22 +0900

    Add simple-filter for android-logcat.

--- koodev/package-init/koodev-android.el
+++ koodev/package-init/koodev-android.el
@@ -9,6 +9,14 @@
         (lambda ()
           (add-to-list 
            'gud-jdb-classpass
-           (expand-file-name "platforms/android-19/android.jar" koodev-android-sdk-path)))))))
+           (expand-file-name "platforms/android-19/android.jar" koodev-android-sdk-path))))
+
+       (define-key android-logcat-map (kbd "F")
+         (lambda (simple-filter)
+           "Set the simple filter of `android-logcat-buffer' to
+REGEXP-FILTER surrounded by a pair of \".*\"s."
+           (interactive "MSimple Filter: ")
+           (android-logcat-set-filter
+            (concat ".*" simple-filter ".*")))))))
 
 ;;; koodev-android.el ends here.
