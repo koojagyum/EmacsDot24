4e71f0ac8db890da0949b00b8c214e5f43acf706
Jagyum Koo <koojagyum@gmail.com>
2014-12-08 15:09:00 +0900

    Add org-publish settings.

--- koodev/package-init/koodev-org.el
+++ koodev/package-init/koodev-org.el
@@ -5,6 +5,28 @@
 
 (eval-after-load 'org
   '(progn
+     ;; org-publish setting
+     (and (require 'ox-publish nil t)
+          (setq org-publish-project-alist
+                ;; [FIXME] shuld be replaced with cutomized variables
+                '(("koodev-org"
+                   :base-directory "~/Workspace/private/document/org/org-article"
+                   :base-extension "org"
+                   :publishing-directory "~/Workspace/private/document/org/org-publish"
+                   :recursive t
+                   :publishing-function org-html-publish-to-html
+                   :headline-levels 4
+                   :html-extension "html"
+                   :auto-sitemap t
+                   :exclude "common.org")
+                  ("koodev-static"
+                   :base-directory "~/Workspace/private/document/org/org-article"
+                   :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|php"
+                   :publishing-directory "~/Workspace/private/document/org/org-publish"
+                   :recursive t
+                   :publishing-function org-publish-attachment)
+                  ("koodev" :components ("koodev-org" "koodev-static")))))
+
      ;; Babel-plantuml setting
      (setq org-plantuml-jar-path
            (expand-file-name "~/.emacs.d/jar/plantuml.jar"))
