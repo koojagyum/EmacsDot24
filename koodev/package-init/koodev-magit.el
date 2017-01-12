2dd0562d4dbcfc51699532f2e3fee7c6ae06e028
Jagyum Koo <koojagyum@gmail.com>
2017-01-12 20:13:10 +0900

    Changes on magit diff color

--- koodev/package-init/koodev-magit.el
+++ koodev/package-init/koodev-magit.el
@@ -6,8 +6,8 @@
   '(progn
      (global-set-key (kbd "C-x g") 'magit-status)
 
-     (set-face-foreground 'magit-diff-add "green3")
-     (set-face-foreground 'magit-diff-del "red3")
+     ;; (set-face-foreground 'magit-diff-add "green3")
+     ;; (set-face-foreground 'magit-diff-del "red3")
      (when (not window-system)
        (set-face-background 'magit-item-highlight "block"))))
 
