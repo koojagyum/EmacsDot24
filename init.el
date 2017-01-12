c5df476a6fa78969fd228224086d24a9f02688fe
Jagyum Koo <koojagyum@gmail.com>
2017-01-12 20:15:54 +0900

    Auto-added code from Package.el

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (defvar emacsdot-dir (file-name-directory load-file-name)
   "The root dir(.emacs.d) of the Emacs configuration.")
 (defvar koodev-dir (expand-file-name "koodev" emacsdot-dir)
