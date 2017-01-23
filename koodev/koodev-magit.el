82c05b6d6e473876b8c5cbbf8196e2e4c0f9e624
Jagyum Koo <koojagyum@gmail.com>
2017-01-23 15:00:34 +0900

    Disable bgcolor setting for magit & terminal

--- koodev/koodev-magit.el
+++ koodev/koodev-magit.el
@@ -2,8 +2,4 @@
  (require 'magit nil t)
  (message "magit initializing..."))
 
-(eval-after-load 'magit
-     (when (not window-system)
-       (set-face-background 'magit-item-highlight "block")))
-
 (provide 'koodev-magit)
