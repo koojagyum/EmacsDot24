63b2507efdc5589cffb4e0ae1f30c35bd170fae3
Jagyum Koo <koodev@Jagyums-MacBook-Pro.local>
2017-03-22 00:53:36 +0900

    Modify additional path variable

--- koodev/koodev-macos.el
+++ koodev/koodev-macos.el
@@ -5,8 +5,8 @@
   ;; About PATH env
   (setq exec-path
         (append
-         '("~/usr/local/bin") exec-path))
-  (setenv "PATH" (concat "~/usr/local/bin:" (getenv "PATH")))
+         '("/usr/local/bin") exec-path))
+  (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
 
   ;; Font setting
   (custom-set-faces '(default ((t (:height 150 :family "Menlo"))))))
