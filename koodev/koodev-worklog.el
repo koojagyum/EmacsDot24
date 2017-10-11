651b6aac1d96beb6a90d43c72e24a6c5f086fd51
Marvin <marvin.koo@snowcorp.com>
2017-10-11 20:41:14 +0900

    Naming about worklog was changed

--- koodev/koodev-worklog.el
+++ koodev/koodev-worklog.el
@@ -2,7 +2,7 @@
 
 (require 'org-capture)
 
-(defcustom koodev-worklog-worklog-path "~/Workspace/Worklogs/Ongoing"
+(defcustom koodev-worklog-path-ongoing "~/Workspace/Worklogs/Ongoing"
   "Default path for recording worklog"
   :type 'string
   :group 'koodev)
@@ -28,7 +28,7 @@
 (defun koodev-worklog-new (name)
   (interactive "sNew workitem name: ")
   (let ((worklog-dir
-         (expand-file-name name koodev-worklog-worklog-path)))
+         (expand-file-name name koodev-worklog-path-ongoing)))
     (let ((worklog-file
            (expand-file-name "worklog.org" worklog-dir)))
       (mkdir worklog-dir)
